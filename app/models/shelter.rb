ad29361c735548d6bfc763cbff414304ca94a319
Michael Evans <michaeljevans113@gmail.com>
2020-07-15 16:20:15 -0600

    Add #average_review_rating and #pet_applications_on_file methods

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,9 +1,17 @@
 class Shelter < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets
   has_many :reviews
 
   def total_pets
     pets.count
+  end
+
+  def average_review_rating
+    reviews.average(:rating).to_f
+  end
+
+  def pet_applications_on_file
+    pets.sum {|pet| pet.applications.count}
   end
 end
